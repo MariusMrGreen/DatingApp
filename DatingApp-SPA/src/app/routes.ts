890b5c30b014fc29aeb869ad0d358239fe984e7b
Marius <Marius@LAPTOP-E5OO5274>
2019-07-16 08:50:11 +0200

    End section 8

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -17,5 +17,5 @@ export const appRoutes: Routes = [
             {path: 'lists', component : ListsComponent}
         ]
     },
-    {path: '**', redirectTo : '', pathMatch: 'full'}
+    { path: '**', redirectTo : '', pathMatch: 'full'}
 ];
